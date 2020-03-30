2b4d1f7fabc8fdc9947d406e84704866b842a0d7
dmattia <david@transcend.io>
2020-03-30 16:32:18 -0500

    Fix sha

--- Formula/terragrunt-atlantis-config.rb
+++ Formula/terragrunt-atlantis-config.rb
@@ -4,7 +4,7 @@ class TerragruntAtlantisConfig < Formula
   url "https://s3-eu-west-1.amazonaws.com/downloads.heft.io/0.0.3/heft_0.0.3_darwin_amd64.zip"
   url "https://homebrew.transcend.io/terragrunt-atlantis-config/0.0.1/terragrunt-atlantis-config_0.0.1_darwin_amd64.zip"
   version "0.0.2"
-  sha256 "959e28cba88a689cac9fd84140d35b04d3d1193cc10b8fd7f0af873a073d4da9"
+  sha256 "6c7a37aa078243d4df48d9bfeb9df4ff5f97f06e442aeaf78589d927e51fc85b"
 
   def install
     bin.install "terragrunt-atlantis-config"
