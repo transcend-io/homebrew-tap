71f26f3f71892f0417888e92f31045c0657adb93
dmattia <david@transcend.io>
2020-04-27 14:24:42 -0500

    Added note on calculating sha256

--- Formula/terragrunt-atlantis-config.rb
+++ Formula/terragrunt-atlantis-config.rb
@@ -3,6 +3,7 @@ class TerragruntAtlantisConfig < Formula
   homepage "https://github.com/transcend-io/terragrunt-atlantis-config"
   url "https://homebrew.transcend.io/terragrunt-atlantis-config/0.0.5/terragrunt-atlantis-config_0.0.5_darwin_amd64.zip"
   version "0.0.5"
+  # Can be calculated with `curl -s <url_from_above> | shasum -a256`
   sha256 "5529ea966e964be9151e3a858a7ef23a60a904b2e1f39926c4b7b00bf2f71b25"
 
   def install
