37da52f1cd2d3f77298c0e8da3bf3992d71e8d5f
David Mattia <8922077+dmattia@users.noreply.github.com>
2020-04-27 14:17:07 -0500

    Dmattia/0.0.5 (#2)
    * Release v0.0.5 of terragrunt-atlantis-config
    
    * fix sha256
    
    Co-authored-by: dmattia <david@transcend.io>

--- Formula/terragrunt-atlantis-config.rb
+++ Formula/terragrunt-atlantis-config.rb
@@ -3,7 +3,7 @@ class TerragruntAtlantisConfig < Formula
   homepage "https://github.com/transcend-io/terragrunt-atlantis-config"
   url "https://homebrew.transcend.io/terragrunt-atlantis-config/0.0.5/terragrunt-atlantis-config_0.0.5_darwin_amd64.zip"
   version "0.0.5"
-  sha256 "7fb3bc99e8458d00e4b29ec924c54e9f44f6c8940d4c87319c66001a731e8c60"
+  sha256 "5529ea966e964be9151e3a858a7ef23a60a904b2e1f39926c4b7b00bf2f71b25"
 
   def install
     bin.install "terragrunt-atlantis-config"
