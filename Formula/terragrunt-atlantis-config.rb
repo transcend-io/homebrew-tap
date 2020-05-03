e42fb04571fc686b1206046200a8c3e80484a042
dmattia <david@transcend.io>
2020-05-03 15:09:03 -0500

    Bump terragrunt-atlantis-config to 0.1.1

--- Formula/terragrunt-atlantis-config.rb
+++ Formula/terragrunt-atlantis-config.rb
@@ -1,10 +1,10 @@
 class TerragruntAtlantisConfig < Formula
   desc "Generates Atlantis Config for Terragrunt projects"
   homepage "https://github.com/transcend-io/terragrunt-atlantis-config"
-  url "https://homebrew.transcend.io/terragrunt-atlantis-config/0.1.0/terragrunt-atlantis-config_0.1.0_darwin_amd64.zip"
-  version "0.1.0"
+  url "https://homebrew.transcend.io/terragrunt-atlantis-config/0.1.1/terragrunt-atlantis-config_0.1.1_darwin_amd64.zip"
+  version "0.1.1"
   # Can be calculated with `curl -s <url_from_above> | shasum -a256`
-  sha256 "45d6192054b8a910866393ab6a860e29954f743566f1702ddd82be787c762fe2"
+  sha256 "b2708c416494ff33b6f4bd4f6a4086a06ea9ccc572dbfcd21b2aea232df99867"
 
   def install
     bin.install "terragrunt-atlantis-config"
