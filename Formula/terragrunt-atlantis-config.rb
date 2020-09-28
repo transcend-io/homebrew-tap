dc0afd08f1407b9c762fdffc086bef4d55cdb75e
Shaun Becker <shaunwbecker@gmail.com>
2020-09-28 12:04:28 -0600

    v0.8.0 Update

--- Formula/terragrunt-atlantis-config.rb
+++ Formula/terragrunt-atlantis-config.rb
@@ -1,10 +1,10 @@
 class TerragruntAtlantisConfig < Formula
   desc "Generates Atlantis Config for Terragrunt projects"
   homepage "https://github.com/transcend-io/terragrunt-atlantis-config"
-  url "https://homebrew.transcend.io/terragrunt-atlantis-config/0.5.0/terragrunt-atlantis-config_0.5.0_darwin_amd64.zip"
-  version "0.5.0"
+  url "https://homebrew.transcend.io/terragrunt-atlantis-config/0.8.0/terragrunt-atlantis-config_0.8.0_darwin_amd64.zip"
+  version "0.8.0"
   # Can be calculated with `curl -s <url_from_above> | shasum -a256`, but only after it is already hosted
-  sha256 "5ff5997826fc642bfbeb12835d7823dfb33396cdf7c3af62d840ee0314a97b35"
+  sha256 "52f1b405dae874c5dde3d99c4ba89254c1506b1ed398722076f4f1fb88fab3d5"
 
   def install
     bin.install "terragrunt-atlantis-config"
