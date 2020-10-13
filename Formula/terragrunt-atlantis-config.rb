a6c1994dd6290d7b17fb739ce088e9b1f46a3008
David Mattia <8922077+dmattia@users.noreply.github.com>
2020-10-13 09:10:19 -0500

    fix sha example command (#12)
    * partial upgrade to 0.9.7
    
    * Fix sha command
    
    Co-authored-by: dmattia <david@transcend.io>

--- Formula/terragrunt-atlantis-config.rb
+++ Formula/terragrunt-atlantis-config.rb
@@ -3,8 +3,8 @@ class TerragruntAtlantisConfig < Formula
   homepage "https://github.com/transcend-io/terragrunt-atlantis-config"
   url "https://homebrew.transcend.io/terragrunt-atlantis-config/0.9.7/terragrunt-atlantis-config_0.9.7_darwin_amd64.zip"
   version "0.9.7"
-  # Can be calculated with `curl -s <url_from_above> | shasum -a256`, but only after it is already hosted
-  sha256 "5ff5997826fc642bfbeb12835d7823dfb33396cdf7c3af62d840ee0314a97b35"
+  # Can be calculated with `curl <url_from_above> --output /tmp/tac && shasum -a256 /tmp/tac | awk '{ print $1 }'`, but only after it is already hosted
+  sha256 "967761ddc1516baf0d308708d25c57b7e7743e5e1c160c5abd92abe03427bda3"
 
   def install
     bin.install "terragrunt-atlantis-config"
