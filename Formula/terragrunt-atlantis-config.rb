2f9b0fa864fdf2766f842dce193209de064352ba
dmattia <david@transcend.io>
2020-12-01 10:30:34 -0600

    update sha of TAC

--- Formula/terragrunt-atlantis-config.rb
+++ Formula/terragrunt-atlantis-config.rb
@@ -4,7 +4,7 @@ class TerragruntAtlantisConfig < Formula
   url "https://homebrew.transcend.io/terragrunt-atlantis-config/0.10.1/terragrunt-atlantis-config_0.10.1_darwin_amd64.zip"
   version "0.10.1"
   # Can be calculated with `curl <url_from_above> --output /tmp/tac && shasum -a256 /tmp/tac | awk '{ print $1 }'`, but only after it is already hosted
-  sha256 "88826204f7f2cda46bc27c8216d5b3210d061dd8d1d6cdb7ceca90af7bced6b6"
+  sha256 "f93257f9ec4323bac26d34c367336583ba11096dabb3636bd50393f0ed16ac9c"
 
   def install
     bin.install "terragrunt-atlantis-config_0.10.1_darwin_amd64" => "terragrunt-atlantis-config"
